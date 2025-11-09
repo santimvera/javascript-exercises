1ae1894e80f5d079e8755baf6cabd8aecf813776
santimvera <87710111+santimvera@users.noreply.github.com>
2025-11-09 18:19:12 +0100

    solve exercise 6: Repeat a String

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (aString, aNumber) {
+    if (aNumber < 0) return 'ERROR';
+    let myString = '';
+    for (let i = 0; i < aNumber; i++) {
+        myString += aString;
+    }
+    return myString
 };
 
 // Do not edit below this line
