7d5db898c971fc56e0766023bd1af4702ed6b52f
santimvera <87710111+santimvera@users.noreply.github.com>
2025-11-09 18:32:26 +0100

    solve exercise 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (aString) {
+    let reversedString = [];
+    for (const char of aString) { reversedString.unshift(char); }
+    return reversedString.reduce((sum, current) => sum + current, '');
 };
 
 // Do not edit below this line
