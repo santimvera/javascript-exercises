eaf83b7ca6c123f36dde4a2f2031d92d8d217a9f
santimvera <87710111+santimvera@users.noreply.github.com>
2025-11-09 18:54:26 +0100

    solve exercise 09: sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function (numberOne, numberTwo) {
+    if (numberOne === numberTwo) return numberOne;
+    if (numberOne < 0 || numberTwo < 0) return 'ERROR';
+    if (!Number.isInteger(numberOne) || !Number.isInteger(numberTwo)) return 'ERROR';
+    if (typeof numberOne !== 'number' || typeof numberTwo !== 'number') return 'ERROR';
+    
+    let orderedNumbers = [numberOne, numberTwo].sort((a, b) => a - b);
+    let allNumbers = [];
+    for (let i = orderedNumbers[0]; i <= orderedNumbers[1]; i++) {
+        allNumbers.push(i);
+    }
+    const sum = allNumbers.reduce((total, current) => total + current, 0);
+    return sum;
+
 
 };
 
