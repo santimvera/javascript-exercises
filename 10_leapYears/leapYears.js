20e88205efe551028851f3d684eaca187fed7708
santimvera <87710111+santimvera@users.noreply.github.com>
2025-11-09 18:59:39 +0100

    solve exercise 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(aNumber) {
+    if (aNumber % 4 === 0) {
+        if (aNumber % 100 === 0) {
+            if (aNumber % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }   
 
 };
 
